2bbfe7e73622f2a156b546cc494e59aadcd210ae
hanumaan3 <52711060+hanumaan3@users.noreply.github.com>
2019-07-09 12:09:20 -0400

    Add files via upload

---
+++ DockerTest.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class DockerTest{
+	public static void main(String args[]){
+		
+		
+		System.out.println("****************************************WELCOME!****************************************\n");
+	}
+	
+}
---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM java:8
+WORKDIR /
+ADD DockerTest.jar DockerTest.jar
+EXPOSE 8000
+CMD ["java", "-jar", "DockerTest.jar"]
+
